2448748638f9006543a4cd6e6ce45300d0664d26
Tarun Thummala <tarunt815@gmail.com>
2019-06-22 13:45:25 -0400

    Forgot some files from the latest commit

--- src/index.js
+++ src/index.js
@@ -6,17 +6,24 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { createAppContainer } from "react-navigation";
-import NavigationService from "./utils/NavigationService";
-import TopLevelNavigator from "./screens";
+import React, { Component } from 'react';
+import { createAppContainer } from 'react-navigation';
+import NavigationService from './utils/NavigationService';
+import TopLevelNavigator from './screens';
 
-import { Provider } from "react-redux";
-import store from "Homecooked/src/modules";
+import { Provider } from 'react-redux';
+import store from 'Homecooked/src/modules';
+
+import { UrbanAirship } from 'urbanairship-react-native';
 
 const AppContainer = createAppContainer(TopLevelNavigator);
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        UrbanAirship.setUserNotificationsEnabled(true);
+    }
+
     render() {
         return (
             <Provider store={store}>
