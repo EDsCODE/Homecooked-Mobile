b92933cf62b9e57c6e9fc8b5c7d5a2ab41a347a5
Tarun Thummala <tarunt815@gmail.com>
2019-06-25 22:47:53 -0400

    Added icons to app

--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -1,8 +1,11 @@
-import { createBottomTabNavigator } from "react-navigation";
-import Feed from "./feed";
-import HistoryStack from "./history";
-import Notifications from "./notifications";
-import Account from "./account";
+import { createBottomTabNavigator } from 'react-navigation';
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import Feed from './feed';
+import HistoryStack from './history';
+import Notifications from './notifications';
+import Account from './account';
+import { Color } from 'Homecooked/src/components/styles';
 
 const MainStack = createBottomTabNavigator(
     {
@@ -20,8 +23,38 @@ const MainStack = createBottomTabNavigator(
         }
     },
     {
-        initialRouteName: "Account"
-    }
+        initialRouteName: 'Account',
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                const { routeName } = navigation.state;
+                var imgSource;
+                if (routeName === 'Feed') {
+                    // Sometimes we want to add badges to some icons.
+                    // You can check the implementation below.
+                    imgSource = require('Homecooked/src/assets/img/GOpenTables.png');
+                } else if (routeName === 'History') {
+                    imgSource = require('Homecooked/src/assets/img/GMyTables.png');
+                } else if (routeName === 'Notification') {
+                    imgSource = require('Homecooked/src/assets/img/GHNotifs.png');
+                } else if (routeName === 'Account') {
+                    imgSource = require('Homecooked/src/assets/img/GHProfile.png');
+                }
+
+                // You can return any component that you like here!
+                return (
+                    <Image
+                        source={imgSource}
+                        style={{ width: 25, height: 25, tintColor }}
+                    />
+                );
+            }
+        }),
+        tabBarOptions: {
+            showIcon: true,
+            activeTintColor: '#FF674F'
+        }
+    },
+    {}
 );
 
 export default MainStack;
