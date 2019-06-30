1866fb5ec8d216d436a17301a472f4c1a9e9742f
Tarun Thummala <tarunt815@gmail.com>
2019-06-30 11:12:37 -0400

    fixed edit bio

--- src/screens/main/account/Profile/EditBio.js
+++ src/screens/main/account/Profile/EditBio.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
-import { View, FlatList, Text, StyleSheet } from "react-native";
-import { NavigationActions } from "react-navigation";
-import HeadingText from "Homecooked/src/components/Text/Heading";
-import PromptText from "Homecooked/src/components/Text/Prompt";
-import CloseButton from "Homecooked/src/components/Buttons/Close";
-import BarButton from "Homecooked/src/components/Buttons/BarButton";
-import NavigationService from "Homecooked/src/utils/NavigationService";
-import { Spacing, Typography, Color } from "Homecooked/src/components/styles";
-import TextField from "Homecooked/src/components/TextFields/Material";
+import React, { Component } from 'react';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+import HeadingText from 'Homecooked/src/components/Text/Heading';
+import PromptText from 'Homecooked/src/components/Text/Prompt';
+import CloseButton from 'Homecooked/src/components/Buttons/Close';
+import BarButton from 'Homecooked/src/components/Buttons/BarButton';
+import NavigationService from 'Homecooked/src/utils/NavigationService';
+import { Spacing, Typography, Color } from 'Homecooked/src/components/styles';
+import TextField from 'Homecooked/src/components/TextFields/Material';
 
-import { currentUserTypes } from "Homecooked/src/modules/types";
-import { connect } from "react-redux";
+import { currentUserTypes } from 'Homecooked/src/modules/types';
+import { connect } from 'react-redux';
 
 const placeHolderWidth = 140;
 
 class Bio extends Component {
     state = {
-        bio: ""
+        bio: ''
     };
     _goBack = () => {
         this.props.navigation.goBack();
@@ -28,11 +28,11 @@ class Bio extends Component {
                 bio: this.state.bio
             });
         }
-        this.props.navigation.navigate("ProfilePreview", {
+        this.props.navigation.navigate('ProfilePreview', {
             profileImageSignedUrl: this.props.currentUser.profileImageSignedUrl,
             bio: this.state.bio,
             firstName: this.props.currentUser.firstName,
-            returnRoute: "AccountMain"
+            returnRoute: 'AccountMain'
         });
     };
 
@@ -42,7 +42,7 @@ class Bio extends Component {
         } else if (this.props.currentUser.bio) {
             return this.props.currentUser.bio;
         } else {
-            return "";
+            return '';
         }
     };
 
@@ -50,11 +50,10 @@ class Bio extends Component {
         let { bio } = this.state;
         return (
             <View style={styles.container}>
-                <CloseButton onPress={this._goBack} icon={"arrow-round-back"} />
+                <CloseButton onPress={this._goBack} icon={'arrow-round-back'} />
                 <HeadingText>Write a Bio</HeadingText>
                 <PromptText style={{ marginTop: Spacing.small }}>
-                    Each guest brings a unique flavor to the Homecooked
-                    experience.
+                    Each guest brings a unique flavor to the gathr experience.
                 </PromptText>
                 <TextField
                     tintColor={Color.gray}
@@ -69,7 +68,7 @@ class Bio extends Component {
                 <BarButton
                     title="Complete Profile"
                     style={{
-                        position: "absolute",
+                        position: 'absolute',
                         bottom: Spacing.large,
                         left: Spacing.large
                     }}
@@ -116,10 +115,10 @@ const styles = StyleSheet.create({
         marginTop: Spacing.small,
         width: placeHolderWidth * 2 + 20,
         height: placeHolderWidth * 2 + 20,
-        alignSelf: "center",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-around",
-        alignContent: "space-around"
+        alignSelf: 'center',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        alignContent: 'space-around'
     }
 });
